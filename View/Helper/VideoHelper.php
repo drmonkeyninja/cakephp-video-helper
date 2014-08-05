365882689fcb99f4c401da5f011b92135c45a62a
Andy Carter <drmonkeyninja@gmail.com>
2014-08-05 16:27:58 +0100

    Converted into a plugin.

--- View/Helper/VideoHelper.php
+++ View/Helper/VideoHelper.php
@@ -1,14 +1,12 @@
 <?php
-
 /**
  * CakePHP helper for embedding YouTube and Vimeo videos.
  * 
  * @name       Video Helper
  * @author     Andy Carter (@drmonkeyninja)
  * @author     Emerson Soares (dev.emerson@gmail.com)
- * @version    2.0
+ * @version    2.0.0.0
  * @license    MIT License (http://www.opensource.org/licenses/mit-license.php) 
- * 
  */
 class VideoHelper extends HtmlHelper {
 
