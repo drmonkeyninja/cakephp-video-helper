097bee89d2295cfca21f2a4f21271fd29f75ef94
Andy Carter <drmonkeyninja@gmail.com>
2014-08-08 11:14:21 +0100

    Ensured URLs use forward slashes.

--- View/Helper/VideoHelper.php
+++ View/Helper/VideoHelper.php
@@ -40,7 +40,7 @@ class VideoHelper extends HtmlHelper {
 
 		$settings = array_merge($default_settings, $settings);
 		$video_id = $this->_getVideoId($url);
-		$settings['src'] = $this->_apis['youtube'] . DS . 'embed' . DS . $video_id . '?hd=' . $settings['hd'];
+		$settings['src'] = $this->_apis['youtube'] . '/' . 'embed' . '/' . $video_id . '?hd=' . $settings['hd'];
 
 		return $this->tag('iframe', null, array(
 					'width' => $settings['width'],
@@ -68,7 +68,7 @@ class VideoHelper extends HtmlHelper {
 		$settings = array_merge($default_settings, $settings);
 
 		$video_id = $this->_getVideoId($url);
-		$settings['src'] = $this->_apis['vimeo'] . DS . $video_id . '?title=' . $settings['show_title'] . '&amp;byline=' . $settings['show_byline'] . '&amp;portrait=' . $settings['show_portrait'] . '&amp;color=' . $settings['color'] . '&amp;autoplay=' . $settings['autoplay'] . '&amp;loop=' . $settings['loop'];
+		$settings['src'] = $this->_apis['vimeo'] . '/' . $video_id . '?title=' . $settings['show_title'] . '&amp;byline=' . $settings['show_byline'] . '&amp;portrait=' . $settings['show_portrait'] . '&amp;color=' . $settings['color'] . '&amp;autoplay=' . $settings['autoplay'] . '&amp;loop=' . $settings['loop'];
 		return $this->tag('iframe', null, array(
 					'src' => $settings['src'],
 					'width' => $settings['width'],
@@ -185,9 +185,9 @@ class VideoHelper extends HtmlHelper {
 			'large' => 0, // 480px x 360px 
 			'thumb1' => 1, // 120px x 90px at position 25% 
 			'thumb2' => 2, // 120px x 90px at position 50% 
-			'thumb3' => 3  // 120px x 90px at position 75% 
+			'thumb3' => 3  // 120px x 90px at position 75%
 		);
-		$image_url = $this->_apis['youtube_image'] . DS . $video_id . DS . $accepted_sizes[$size] . '.jpg';
+		$image_url = $this->_apis['youtube_image'] . '/' . $video_id . '/' . $accepted_sizes[$size] . '.jpg';
 		return $this->image($image_url, $options);
 
 	}
