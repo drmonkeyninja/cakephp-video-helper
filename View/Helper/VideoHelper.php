cb5dce0791ee5c14c60da8366be3d74dd6f23ea3
Tim Jackson <tim@evoluted.net>
2017-11-08 19:19:43 +0000

    Part 2: youtu.be support

--- View/Helper/VideoHelper.php
+++ View/Helper/VideoHelper.php
@@ -273,8 +273,8 @@ class VideoHelper extends HtmlHelper {
 
 		switch ($source) {
 			case 'youtube':
-				$params = $this->_getUrlParams($url);
-				return (isset($params['v']) ? $params['v'] : $url);
+				preg_match("/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/", $url, $matches);
+				return (!emptyZ($matches[1]) ? $matches[1] : $url);
 			case 'vimeo':
 				$path = parse_url($url, PHP_URL_PATH);
 				return substr($path, 1);
